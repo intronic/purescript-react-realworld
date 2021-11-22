06fe24a8354cdaf38414275471c27f1bbcadf2a8
Michael Pheasant <mike@pheasant.co.nz>
2021-11-23 03:10:58 +1000

    add types

--- src/Conduit/Root.purs
+++ src/Conduit/Root.purs
@@ -1,6 +1,9 @@
-module Conduit.Root where
+module Conduit.Root
+  ( mkRoot
+  ) where
 
 import Prelude
+
 import Conduit.Capability.Auth (class MonadAuth)
 import Conduit.Capability.Auth as Auth
 import Conduit.Capability.Halo (class MonadHalo, component)
@@ -24,7 +27,12 @@ import Conduit.Page.Register (mkRegisterPage)
 import Conduit.Page.Settings (mkSettingsPage)
 import Control.Monad.State (modify_)
 import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Class (class MonadEffect)
+-- import Effect.Class.Console as Console
+import React.Basic.Hooks (JSX, Render)
 import React.Basic.Hooks as React
+import React.Halo (HaloM, Lifecycle)
 import React.Halo as Halo
 
 data Action
@@ -33,10 +41,16 @@ data Action
   | UpdateRoute Route
   | Navigate Route
 
+type AppState =
+  { auth :: Maybe Auth
+  , route :: Route
+  }
+
 mkRoot ::
   forall m.
   MonadAuth m =>
   MonadRouting m =>
+  MonadEffect m =>
   TagRepository m =>
   ArticleRepository m =>
   MonadHalo m =>
@@ -47,65 +61,96 @@ mkRoot ::
 mkRoot = do
   render <- mkRender
   component "Root" { context, initialState, eval, render }
-  where
-  context _ = pure unit
 
-  initialState _ _ =
-    { auth: Nothing
-    , route: Error
-    }
+eval :: forall props ctx m.
+  MonadAuth m =>
+  MonadRouting m =>
+  Lifecycle props ctx Action
+  -> HaloM props ctx AppState Action m Unit
+eval =
+  Halo.mkEval
+    _
+      { onInitialize = \_ -> Just Initialize
+      , onAction = handleAction
+      }
 
-  eval =
-    Halo.mkEval
-      _
-        { onInitialize = \_ -> Just Initialize
-        , onAction = handleAction
-        }
+context :: forall props. props -> Render Unit Unit Unit
+context _ = pure unit
 
-  handleAction = case _ of
-    Initialize -> do
-      -- auth
-      handleAction <<< UpdateAuth =<< Auth.read
-      Auth.subscribe UpdateAuth
-      -- routing
-      handleAction <<< UpdateRoute =<< Routing.read
-      Routing.subscribe UpdateRoute
-    UpdateAuth auth -> modify_ _ { auth = auth }
-    UpdateRoute route -> do
-      modify_ _ { route = route }
-      auth <- Auth.read
-      case route, auth of
-        Login, Just _ -> Routing.redirect Home
-        Register, Just _ -> Routing.redirect Home
-        Settings, Nothing -> Routing.redirect Home
-        CreateArticle, Nothing -> Routing.redirect Home
-        UpdateArticle _, Nothing -> Routing.redirect Home
-        Error, _ -> Routing.redirect Home
-        _, _ -> pure unit
-    Navigate route -> Routing.navigate route
+initialState :: forall props ctx. props -> ctx -> AppState
+initialState _ _ =
+  { auth: Nothing
+  , route: Error
+  }
 
-  mkRender = do
-    homePage <- mkHomePage
-    loginPage <- mkLoginPage
-    registerPage <- mkRegisterPage
-    settingsPage <- mkSettingsPage
-    editorPage <- mkEditorPage
-    articlePage <- mkArticlePage
-    profilePage <- mkProfilePage
-    pure
-      $ \{ state, send } ->
-          React.fragment
-            [ Header.header { auth: state.auth, currentRoute: state.route, onNavigate: send <<< Navigate }
-            , case state.route of
-                Home -> homePage unit
-                Login -> loginPage unit
-                Register -> registerPage unit
-                Settings -> settingsPage unit
-                CreateArticle -> editorPage { slug: Nothing }
-                UpdateArticle slug -> editorPage { slug: Just slug }
-                ViewArticle slug -> articlePage { slug }
-                Profile username -> profilePage { username, tab: Published }
-                Favorites username -> profilePage { username, tab: Favorited }
-                Error -> React.empty
-            , Footer.footer
-            ]
+
+handleAction :: forall m props ctx state.
+      MonadAuth m =>
+      MonadRouting m =>
+      Action -> HaloM props ctx {auth :: Maybe Auth, route :: Route | state} Action m Unit
+handleAction = case _ of
+  Initialize -> do
+    -- auth
+    handleAction <<< UpdateAuth =<< Auth.read
+    Auth.subscribe UpdateAuth
+    -- routing
+    handleAction <<< UpdateRoute =<< Routing.read
+    Routing.subscribe UpdateRoute
+  UpdateAuth auth -> modify_ _ { auth = auth }
+  UpdateRoute route -> do
+    modify_ _ { route = route }
+    auth <- Auth.read
+    case route, auth of
+      Login, Just _ -> Routing.redirect Home
+      Register, Just _ -> Routing.redirect Home
+      Settings, Nothing -> Routing.redirect Home
+      CreateArticle, Nothing -> Routing.redirect Home
+      UpdateArticle _, Nothing -> Routing.redirect Home
+      Error, _ -> Routing.redirect Home
+      _, _ -> pure unit
+  Navigate route -> Routing.navigate route
+
+mkRender :: forall m props.
+  Bind m =>
+  MonadEffect m =>
+  MonadAuth m =>
+  MonadRouting m =>
+  TagRepository m =>
+  ArticleRepository m =>
+  MonadHalo m =>
+  UserRepository m =>
+  CommentRepository m =>
+  ProfileRepository m =>
+  m
+  ({ send :: Action -> Effect Unit
+  , state ::  AppState
+  | props
+  }
+  -> JSX
+  )
+mkRender = do
+  -- Console.log "Render app"
+  homePage <- mkHomePage
+  loginPage <- mkLoginPage
+  registerPage <- mkRegisterPage
+  settingsPage <- mkSettingsPage
+  editorPage <- mkEditorPage
+  articlePage <- mkArticlePage
+  profilePage <- mkProfilePage
+  pure
+    $ \{ state, send } ->
+        React.fragment
+          [ Header.header { auth: state.auth, currentRoute: state.route, onNavigate: send <<< Navigate }
+          , case state.route of
+              Home -> homePage unit
+              Login -> loginPage unit
+              Register -> registerPage unit
+              Settings -> settingsPage unit
+              CreateArticle -> editorPage { slug: Nothing }
+              UpdateArticle slug -> editorPage { slug: Just slug }
+              ViewArticle slug -> articlePage { slug }
+              Profile username -> profilePage { username, tab: Published }
+              Favorites username -> profilePage { username, tab: Favorited }
+              Error -> React.empty
+          , Footer.footer
+          ]
